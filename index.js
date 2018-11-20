2b5bb7789ede549350df7a71781f1f684e2dc1ce
Eder Rogério <ederrdls@hotmail.com>
2018-11-19 22:26:16 -0200

    conectar com o banco de dados, configurar busca por palavras chave, criar tela de adm para insrir dados no banco

--- index.js
+++ index.js
@@ -6,12 +6,14 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const db = require('express-mongo-db')
 
 const app = express();
 app.use(bodyParser.urlencoded());
 
 app.set('view engine','ejs');
 
+app.use(db("mongodb://localhost:27017/saude"))
 app.use('/assets', express.static('assets'));
 
 // estes dados são enviados na lista. Aqui é apenas um exemplo. Será inserido nesta variável
@@ -25,31 +27,29 @@ app.get('/', (req, res)=>{
 app.post('/', (req, res)=>{
     console.log(req.body.item);
     console.log(req.body.seletor);
-    fs.readFile("dados.csv",{encoding: 'utf-8'}, function(erro, dados){
-       let linhas = dados.split('\n');
-       let hospitais = [];
 
-       for(linha of linhas){
-           let colunas = linha.split(',');
-           let imagem = colunas[0];
-           let nome = colunas[1];
-           hospitais.push({
-               'imagem': imagem,
-                'nome': nome
-        });
-       }
-    //     let hospitais = [];
-    //     for(let dado of dados){
-    //         hospitais.push({
-    //             'imagem': imagem,
-    //              'nome': nome
-    //             });
-    //     }
-    res.render('index', {'lista': hospitais});
-    });
-
+    req.db.collection('hospitais').find({descricao:req.body.item.toUpperCase()}).toArray((erro, dados)=>{
+        if(erro){
+            res.send('<h1 style="fontSize:300px">Erro</h1>');
+        }
+        console.log(dados);
+        res.render('index', {lista: dados});
+    })
 
 });
+
+app.get("/adm", (req,res)=>{
+    res.render("adm");
+});
+
+app.post("/adm", (req,res)=>{
+    req.db.collection('hospitais').insert({
+        nome:       req.body.nome.toUpperCase(),
+        img:        req.body.img,
+        descricao:  req.body.descricao.toUpperCase()
+        });
+    res.render("adm");
+})
 
 
 app.listen(3000, () => {
